d95d6c93ba7eb3ec4da0b00a6d5a35ef221b2656
anascen <webovein@gmail.com>
2024-05-14 23:11:23 +0530

    fix: locations

--- apps/engine/src/main.rs
+++ apps/engine/src/main.rs
@@ -11,11 +11,10 @@ async fn main() {
     
     let app = routes::initial_routes::initialize_routes().nest("/v1", routes::v1::router());
     
-    let addr: &str = "0.0.0.0:8080";
+    let addr: &str = "0.0.0.0:3110";
     let listener = tokio::net::TcpListener::bind::<&str>(addr).await.unwrap();
     
     println!("Server is running on port {}", addr);
-    repository::gen_ai::generate_response_from_prompt(String::from("Hello")).await;
     
     axum::serve(listener, app).await.unwrap();
 }
