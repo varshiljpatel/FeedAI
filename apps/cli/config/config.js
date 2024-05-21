981673fd254b7e059ea10a11729e66d5677d246b
Varshil J Patel <123259416+varshiljpatel@users.noreply.github.com>
2024-05-21 10:28:50 +0530

    delete: apps/cli directory

--- apps/cli/config/config.js
+++
@@ -1,8 +0,0 @@
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
-
-const configMut = {
-	openaiKey: process.env.OPEN_AI_KEY,
-};
-
-export default config = Object.freeze(configMut);
